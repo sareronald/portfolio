ec767f9bd78dfa768fd30975da023b4fc658e1f8
Sarah Ronald <sareronald@hotmail.com>
2021-01-21 21:46:30 +1100

    Footer and About me pages uodated

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer className="text-center py-3 mt-auto">
+      <p>&copy; Copyright January 2021</p>
+    </footer>
+  );
+};
+
+export default Footer;
