cf7d9419d5c37f18c7b723c652261bfc9a1513ec
Sarah Ronald <sareronald@hotmail.com>
2021-01-21 18:57:02 +1100

    initial commit of portfolio app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from "./logo.svg";
+import Jumbotron from "./components/Jumbotron";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Jumbotron />
+      <About />
+    </div>
+  );
+}
+
+export default App;
